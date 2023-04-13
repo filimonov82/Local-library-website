756b129de29a68b406062c907812016e8069cb1f
filimonov82 <andr7295@gmail.com>
2023-04-13 19:27:55 +0300

    Set up admin site view and add a new model

--- catalog/models.py
+++ catalog/models.py
@@ -4,10 +4,17 @@ import uuid  # Required for unique book instances
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction, French Poetry etc.)')
 
     def __str__(self):
         # String for representing the Model object (in Admin site etc.)
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a book language (e.g. Russian, English)')
+
+    def __str__(self):
         return self.name
 
 
@@ -16,11 +23,17 @@ class Book(models.Model):
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file.
-    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
     def __str__(self):
         return self.title
@@ -55,7 +68,7 @@ class BookInstance(models.Model):
         ordering = ['due_back']
 
     def __str__(self):
-        """String for representing the Model object."""
+        # String for representing the Model object.
         return f'{self.id} ({self.book.title})'
 
 
@@ -69,7 +82,7 @@ class Author(models.Model):
         ordering = ['last_name', 'first_name']
 
     def get_absolute_url(self):
-        """Returns the URL to access a particular author instance."""
+        # Returns the URL to access a particular author instance.
         return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
