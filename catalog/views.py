d5e193d0d4f507c4b737c686d207d330bffec989
filimonov82 <andr7295@gmail.com>
2023-04-16 15:45:09 +0300

    Add a homepage
    Created new function for new page in views.py, new URL pattern in
    urls.py, base template, index.html template, style.css.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre, Language
 
-# Create your views here.
+
+def index(request):
+    num_books = Book.objects.count()
+    word_books = Book.objects.filter(title__icontains='не').count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'word_books': word_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres
+    }
+
+    return render(request, 'index.html', context=context)
