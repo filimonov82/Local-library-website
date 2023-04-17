b9bb546a9b951a3b9020de1ae4ff3f7f7d7aa964
filimonov82 <andr7295@gmail.com>
2023-04-17 19:16:30 +0300

    Create ListView for book model

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre, Language
 
 
@@ -20,3 +21,7 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
